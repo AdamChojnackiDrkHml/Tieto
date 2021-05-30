4697d0e5901986600769562cd5c0e335464be3d2
Adam <niepoprawnyetycznie@gmail.com>
2021-05-30 18:52:31 +0200

    Resolve clang warnings, Add Makefile

--- app/main.c
+++ app/main.c
@@ -5,18 +5,22 @@
 #include <pthread.h>
 #include <signal.h>
 
-int fd;
-pthread_t Reader;
+static int fd;
+static pthread_t Reader;
 
-void * _task_reader()
+static void*  _task_reader(void *arg);
+static void*  _task_reader(void *arg)
 {
-    
+    char* parameter = (char*)arg;
+    write(0, "Reader started with paramter ", 29);
+    write(0, parameter, 1);
+    write(0, "\n", 1);
     
     while(1)
     {
         fd = open("/proc/stat", O_RDONLY);
         char c;
-        int bytes;
+        long bytes;
         while((bytes = read(fd, &c, sizeof(c))) > 0) {
             write(0, &c, sizeof(char));
         }
@@ -25,18 +29,20 @@ void * _task_reader()
     }
 }
 
-void handler()
+static void handler(int n)
 {
+    n += 0x30;
     close(fd);
     write(0, "\nProgram End\n", 13);
+    write(0, &n, 1);
     pthread_cancel(Reader);
 }
 
 int main(void)
 {
-    signal(SIGTERM, handler);
+    signal(SIGTERM, *handler);
 
-    pthread_create(&Reader, NULL, *_task_reader, NULL);
+    pthread_create(&Reader, NULL, &_task_reader, NULL);
     pthread_join(Reader, NULL);
     
     return 0;
